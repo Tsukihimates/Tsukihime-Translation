eb6a5fa5ef352d0c66bc4a0ecdca2f4ad5ab0337
Ross Schlaikjer <ross@schlaikjer.net>
2021-12-04 09:15:48 -0500

    Sed out trailing spaces

--- script/1_0_2current_en_script/remove_verboten_punctuation.sh
+++ script/1_0_2current_en_script/remove_verboten_punctuation.sh
@@ -10,3 +10,6 @@ find . -type f -name *.txt -exec sed -i 's/…/.../g' {} \;
 # No unicode quote marks
 find . -type f -name *.txt -exec sed -i 's/“/"/g' {} \;
 find . -type f -name *.txt -exec sed -i 's/”/"/g' {} \;
+
+# Strip trailing whitespace on script files
+find . -type f -name *.txt -exec sed -Ei 's/[[:blank:]]+$//' {} \;
