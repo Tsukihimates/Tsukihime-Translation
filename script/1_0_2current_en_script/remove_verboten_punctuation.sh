529ba9799274ce535e604d1d805281570a6d0872
Ross Schlaikjer <ross@schlaikjer.net>
2021-12-04 11:37:00 -0500

    Warn about unspaced ruby

--- script/1_0_2current_en_script/remove_verboten_punctuation.sh
+++ script/1_0_2current_en_script/remove_verboten_punctuation.sh
@@ -13,3 +13,11 @@ find . -type f -name *.txt -exec sed -i 's/”/"/g' {} \;
 
 # Strip trailing whitespace on script files
 find . -type f -name *.txt -exec sed -Ei 's/[[:blank:]]+$//' {} \;
+
+# Detect files with condensed ruby
+set +x
+BAD_RUBY=`find . -type f -exec grep '|[[:alpha:]][[:alpha:]][[:alpha:] ]*>' {} \;`
+if [ ! -z "$BAD_RUBY" ]; then
+  echo "The following files have unspaced ruby:"
+  find . -type f -exec grep --color '|[[:alpha:]][[:alpha:]][[:alpha:] ]*>' {} \;
+fi
