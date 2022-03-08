e17eb317a4ef0b9703c20dad369f5a360baa53a7
Ross Schlaikjer <ross@schlaikjer.net>
2022-03-07 21:38:01 -0500

    Un-doublewiding of certain chars

--- tools/add_font_effects.py
+++ tools/add_font_effects.py
@@ -15,7 +15,12 @@ def pua_range(i):
     )
 
 
-def apply_origin(font, function):
+def apply_origin(font, function, post_scale=None):
+    # Since some of our glyphs get shrunk, we don't want to move them
+    # _all_ the way back to where they started.
+    if not post_scale:
+        post_scale = (1.0, 1.0)
+
     for glyph in font.selection.byGlyphs:
         bb = glyph.boundingBox()
         cx = (bb[2] + bb[0]) / 2
@@ -23,13 +28,24 @@ def apply_origin(font, function):
         translate_center = psMat.translate(-cx, -cy)
         glyph.transform(translate_center)
         function(glyph)
-        translate_back = psMat.translate(cx, cy)
+        translate_back = psMat.translate(
+            cx * post_scale[0], cy * post_scale[1])
         glyph.transform(translate_back)
 
 
 def main():
     # Open the source font
     font = fontforge.open(sys.argv[1])
+
+    # Since we force all chars to single-width, we need to skew down the CJK
+    # characters that we actually do use
+    for char in '℃♡♥―':
+        font.selection.select(("ranges", None), ord(char), ord(char))
+        apply_origin(
+            font,
+            lambda g: g.transform(psMat.scale(0.5, 1)),
+            post_scale=(0.5, 1.0)
+        )
 
     # Copy the base ASCII range
     font.selection.select(("ranges", None), 0, 127)
@@ -45,7 +61,7 @@ def main():
     # Reversed
     font.selection.select(*pua_range(1))
     font.paste()
-    apply_origin(font, lambda g: g.transform(psMat.scale(-0.7, 1)))
+    apply_origin(font, lambda g: g.transform(psMat.scale(-1, 1)))
 
     # Reversed _and_ italics
     font.selection.select(*pua_range(2))
