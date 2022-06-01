2a3907fb29fb6ea9a2a310f5be9fabef82b697c4
ben <Avuxo@users.noreply.github.com>
2022-06-01 16:21:37 -0400

    functional font patch

--- Ryujinx_mods/beatrice/beatrice.s
+++ Ryujinx_mods/beatrice/beatrice.s
@@ -1,15 +1,4 @@
-0x13bdcc
-;; The value of this float constant
-;; will be squared to determine the character advance.
-fmov s8, #0x12
-end
-
-0x13be20
-;; This actually applies the character advance
-fmadd s0, s8, s8, s0
-end
-
-;; Cancel early line wrap.
-0xc37f8
+0xbe874
+; cancel restrictive check for ascii chars
 nop
 end
