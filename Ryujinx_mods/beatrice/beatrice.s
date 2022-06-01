590cfa1b69e35bc660d10f1a6a35d71916d81d65
ben <Avuxo@users.noreply.github.com>
2022-06-01 18:01:08 -0400

    update with case for unicode heart

--- Ryujinx_mods/beatrice/beatrice.s
+++ Ryujinx_mods/beatrice/beatrice.s
@@ -1,4 +1,23 @@
-0xbe874
-; cancel restrictive check for ascii chars
-nop
+; install trampoline
+0xbe86c
+call #0xc4dd8
+; skip old check logic
+cmp w8, #0x00
+beq #0x1fc
+end
+
+; utf32 in w0
+; result in w8 (gets overwritten by ldr immediately)
+; 0x00 is mode 0, 0x01 is mode 1.
+; sure hope this code is as unused as it seems!
+0xc4dd8
+; U+2661 is the heart that noel uses.
+mov w8, #0x2661
+cmp w0, w8
+beq #0x0C
+
+mov w8, #0x01
+ret
+mov w8, #0x00
+ret
 end
