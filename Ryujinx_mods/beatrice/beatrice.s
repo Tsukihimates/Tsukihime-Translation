792d417fe49d271d1a6fad7cdf4d3022293e2b00
ben <Avuxo@users.noreply.github.com>
2022-07-25 22:36:19 -0400

    kerning v3

--- Ryujinx_mods/beatrice/beatrice.s
+++ Ryujinx_mods/beatrice/beatrice.s
@@ -54,7 +54,9 @@ end
 jump #0x153940
 end
 
+;; ------------------------------
 ;; kerning patch
+;; ------------------------------
 0x13bde8
 ;; is current char 'I'?
 cmp w26, #0x49
@@ -78,26 +80,62 @@ fmov s8, #5.0
 nop
 end
 
+
+
+;; ------------------------------
 ;; install trampoline for capturing next character
+;; ------------------------------
 0x146374
-;; jumps to 0x1441a8 (save link for tail-call)
+;; jumps to 0x1441a8
 bl #-0x21CC
 end
 
+;; ------------------------------
 ;; save the relevant bytes from the string into memory
 ;; x0 has string pointer (immutable)
-;; w13 seems safe as scratch TODO: true?
-;; x1 immediately overwritten, save as scratch.
+;; x9 used as base pointer for x8, doesn't seem re-used
+;; ------------------------------
 0x1441a8
-;; capture next character
-;; string pointer in x0
-ldrb w13, [x0, #0x01]
+blr x8
 
-;; store captured character
-adrp x1, #0x19f8000
-add x1, x1, #0x7c7
-strb w13, [x1]
+;; load incremented string pointer and call again.
+mov x13, x0
+blr x8
 
-;; replace original call (no link)
-br x8
+;; save the new char
+adrp x13, #0x19f8000
+add x13, x13, #0x7c7
+str x0, [x13]
+
+b #0x21bc
+end
+
+;; ------------------------------
+;; save string pointer during utf32 peek
+;; ------------------------------
+0x13de80
+sub sp, sp, #0x20
+stp x29, x30, [sp, #0x10]
+add x29, sp, #0x10
+mov x1, x0
+
+add x0, sp, #0x08
+;; 1
+bl #0x14a2c
+
+;; save incremented string pointer
+mov x13, x1
+
+sub x0, x29, #0x04
+add x1, sp, #0x08
+stur wzr, [x29, #-0x04]
+;; 2
+bl #0x14ccc
+ldur x0, [x29, #-0x04]
+ldp x29, x30, [sp, #0x10]
+add sp, sp, #0x20
+nop
+nop
+nop
+ret
 end
