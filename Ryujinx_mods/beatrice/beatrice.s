d99ba5a0f2d6c5a4f3ce14bb1239c1e2f2b8feb3
ben <Avuxo@users.noreply.github.com>
2022-07-25 22:32:44 -0400

    kerning v1

--- Ryujinx_mods/beatrice/beatrice.s
+++ Ryujinx_mods/beatrice/beatrice.s
@@ -53,3 +53,53 @@ end
 0xfbf68
 jump #0x153940
 end
+
+;; kerning patch
+0x13bde8
+;; is current car 'I'?
+cmp w26, #0x49
+b.ne #0x14
+
+;; load offset for next bytes
+;; x0 is loaded into 2 instructions after patch
+;; so can be safely used as scratch.
+adrp x0, #0x19af000
+add x0, x0, #0xda8
+
+;; peek first byte of strign
+ldrb w13, [x0]
+
+;; is peek'd char "'"
+;; TODO: why 0x29 and not 0x27?
+cmp w13, #0x29
+b.ne #0x08
+
+;; apply kerning value
+fmov s8, #5.0
+nop
+end
+
+;; install post trampoline for next_char_as_utf32
+0x13deb0
+b #0x62f8
+end
+
+;; save the relevant bytes from the string into memory
+;; x27 has a lingering reference to the string pointer.
+0x1441a8
+;; capture next character
+;; lingering reference in x27
+;; x27 can also sometimes be NULL or 0x1, so account for that
+cmp x27, #0x10000
+b.lt #0x08
+ldrb w13, [x27, #0x01]
+
+;; store captured character
+adrp x1, #0x19af000
+add x1, x1, #0xda8
+strb w13, [x1]
+
+;; restore sp (overridden instruction)
+add sp, sp, #0x20
+ret
+end
